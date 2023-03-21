d7f79365168e688f0d78f521e53fbf2da19244ef
geofis <joseramon@geografiafisica.org>
2023-03-20 21:37:13 -0400

    ADD distance to water bodies, temperature and precipitation seasonality

---
+++ gee_python/mostra-leyenda.py
@@ -0,0 +1,7 @@
+#from qgis.gui import *
+#root = QgsProject.instance().layerTreeRoot()
+#model = QgsLayerTreeModel(root)
+#view = QgsLayerTreeView()
+#view.setModel(model)
+#view.show()
+iface.legendInterface().setLayerTreeView(QgsLayerTreeView())
